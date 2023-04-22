24de8c4b52284d598402f4a964da04d4e2ce93f6
jasiex01 <hernasj@wp.pl>
2023-04-22 13:47:22 +0200

    Add laboratory code

---
+++ lab01/kod01/kod01.ino
@@ -0,0 +1,14 @@
+//DO NOT COMPILE - FOR INFORMATION ONLY!
+#include <your_header1.h> //Wymagane pliki nagłówkowe
+#include <your_header2.h>
+
+int main(void)
+{
+    //Kod inicjalizacyjny.
+
+    while (true) //Nieskończona pętla
+    {
+
+        //Główny kod programu wykonywany cyklicznie.
+    }
+}
